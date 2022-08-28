9f106206c40b45d396e6e5621618817a6079166e
Steve Cliff <steve@devcloud.guru>
2022-08-28 18:21:21 +0100

    remove old function

--- main.go
+++ main.go
@@ -50,14 +50,6 @@ func onExit() {
 	// Cleaning stuff here.
 }
 
-// func getIcon(s string) []byte {
-// 	b, err := os.ReadFile(s)
-// 	if err != nil {
-// 		fmt.Print(err)
-// 	}
-// 	return b
-// }
-
 func getHosts() []string {
 
 	var hosts []string
